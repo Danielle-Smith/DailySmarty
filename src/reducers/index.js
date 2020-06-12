4e3beb3142a2424bafd2dace8964bbbcf44ed992
Danielle <danielle@natesdesign.com>
2020-06-12 12:03:13 -0600

    setup results component and route

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 
 const rootReducer = combineReducers({
-  form
+  form,
+  
 });
 
 export default rootReducer;
